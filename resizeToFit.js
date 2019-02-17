89300af380bbcfef839765acee49e745d5ce8fde
mnater <mathiasnater@gmail.com>
2019-02-17 23:04:09 +0100

    lint

--- resizeToFit.js
+++ resizeToFit.js
@@ -1,7 +1,15 @@
-window.resizeToFit = (function makeResizeToFit(){
+window.resizeToFit = (function makeResizeToFit() {
+    "use strict";
     const charges = [];
     let styleSheet = null;
 
+    /**
+     * Adds or replaces a rule.
+     * @param {string} selector - The selector for the rule
+     * @param {string} properties - The properties to be set
+     * @param {number} replaceIdx - The index of the rule this rule replaces
+     * @returns {number} - The new index
+     */
     function setCSS(selector, properties, replaceIdx) {
         if (replaceIdx) {
             styleSheet.deleteRule(replaceIdx);
@@ -10,6 +18,14 @@ window.resizeToFit = (function makeResizeToFit(){
         return idx;
     }
 
+    /**
+     * Reduce font-size until the elements scrollWidth is equal or smaller
+     * than the elements client width.
+     * Getting element.scrollWidth forces a layout. This is really bad
+     * for performance.
+     * @param {array} charge - An array of element and selector
+     * @returns {undefined}
+     */
     function interpolateOptimalFontSize(charge) {
         const element = charge[0];
         const selector = charge[1];
@@ -24,25 +40,41 @@ window.resizeToFit = (function makeResizeToFit(){
         }
     }
 
+    /**
+     * Prepare element for resizing.
+     * @param {array} charge - An array of element and selector
+     * @returns {undefined}
+     */
     function resizeContent(charge) {
-        //unit is always "px" for computed values
+        // The unit is always "px" for computed values
         const element = charge[0];
         const selector = charge[1];
         const computedStyles = window.getComputedStyle(element);
-        const overflowIdx = setCSS(selector, "overflow: hidden")
+        const overflowIdx = setCSS(selector, "overflow: hidden");
         if (!element.startFontSize) {
-            element.startFontSize = parseInt(computedStyles.fontSize.slice(0, -2), 10);
+            element.startFontSize = parseInt(
+                computedStyles.fontSize.slice(0, -2),
+                10
+            );
         }
         interpolateOptimalFontSize(charge);
         styleSheet.deleteRule(overflowIdx);
     }
 
+    /**
+     * Resize each element
+     */
     function resize() {
-        charges.forEach(function (charge) {
+        charges.forEach(function eachCharge(charge) {
             resizeContent(charge);
         });
     }
 
+    /**
+     * Create a styleSheet and collect all elements. Then call resize().
+     * @param {array} selectors - An array of selectors that need to be resized
+     * @returns {undefined}
+     */
     function init(selectors) {
         const styleEl = document.createElement("style");
         styleEl.id = "resizeToFit_Styles";
@@ -50,9 +82,9 @@ window.resizeToFit = (function makeResizeToFit(){
         document.head.appendChild(styleEl);
         styleSheet = styleEl.sheet;
 
-        selectors.forEach(function (selector) {
+        selectors.forEach(function eachSelector(selector) {
             const nodeList = document.querySelectorAll(selector);
-            nodeList.forEach(function (element) {
+            nodeList.forEach(function eachElement(element) {
                 charges.push([element, selector]);
             });
         });
@@ -62,5 +94,5 @@ window.resizeToFit = (function makeResizeToFit(){
     return {
         init,
         resize
-    }
+    };
 }());
