85515a6e736e0b18450570d53b426158d50826fa
mnater <mathiasnater@gmail.com>
2019-03-05 11:15:46 +0100

    Document code

--- resizeToFit.js
+++ resizeToFit.js
@@ -9,8 +9,14 @@
 
 window.resizeToFit = (function makeResizeToFit() {
     "use strict";
+
+    // Storage for elements to be fitted
     const charges = [];
+
+    // The cssHandler-object, created by init()
     let cssHandler = null;
+
+    // Map from selectors to current font-size
     const resizedSelectors = new Map();
 
     /**
@@ -18,6 +24,7 @@ window.resizeToFit = (function makeResizeToFit() {
      * @param {CSSStyleSheet} styleSheet - An existing CSSOM styleSheet
      */
     function makeCSSHandler(styleSheet) {
+        // Map selectors to rule-IDs
         const sel2Id = new Map();
 
         /**
@@ -77,13 +84,21 @@ window.resizeToFit = (function makeResizeToFit() {
      * @returns {undefined}
      */
     function resizeContent(el, selector) {
+        // Previously calculated font-size for the same selector are stored in the `resizedSelectors`-Map.
         const currentFontSize = resizedSelectors.has(selector)
             ? resizedSelectors.get(selector)
             : el.originalFontSize;
+
+        // Set properties to get accurate results for clientWidth and scrollWidth
         cssHandler.setProp(
             selector,
             ["overflow: hidden", "display: block", "font-size: " + el.originalFontSize + "px"]
         );
+
+        /*
+         * Calculate the font-size proportionally to clientWidth and scrollWidth
+         * but don't grow bigger than originalFontSize or currentFontSize
+         */
         const fontSize = Math.min(
             Math.min(
                 el.clientWidth / el.scrollWidth * el.originalFontSize,
@@ -91,17 +106,25 @@ window.resizeToFit = (function makeResizeToFit() {
             ),
             currentFontSize
         );
+
+        // Store font-size for this selector
         resizedSelectors.set(selector, fontSize);
+
+        // Set font-size for that element
         cssHandler.setProp(selector, ["font-size: " + fontSize + "px"]);
+
+        // Remove properties needed for calculation, restoring original values
         cssHandler.deleteProp(selector, ["overflow", "display"]);
     }
 
     /**
-     * Resize each element.
+     * Resize each element previously collected by init
      * @returns {undefined}
      */
     function resize() {
+        // Clear the map that stores current font-sizes for selectors
         resizedSelectors.clear();
+        // Call resizeContent() for each element in charges
         charges.forEach(function eachCharge(charge) {
             resizeContent(charge[0], charge[1]);
         });
@@ -113,21 +136,26 @@ window.resizeToFit = (function makeResizeToFit() {
      * @returns {undefined}
      */
     function init(selectors) {
+        // Let's create a new style sheet where we put the styles
         const styleEl = document.createElement("style");
         styleEl.id = "resizeToFit_Styles";
         styleEl.type = "text/css";
         document.head.appendChild(styleEl);
+
+        // Create cssHandler to easily manipulate the style sheet
         cssHandler = makeCSSHandler(styleEl.sheet);
+
+        // Find all elements according to user provided selectors
         selectors.forEach(function eachSelector(selector) {
             const nodeList = document.querySelectorAll(selector);
             nodeList.forEach(function eachElement(element) {
+                // Save original font-size to each element object
                 const computedStyles = window.getComputedStyle(element);
                 const elementCurrentFontSize = parseFloat(
                     computedStyles.fontSize
                 );
-                if (!element.originalFontSize) {
-                    element.originalFontSize = elementCurrentFontSize;
-                }
+                element.originalFontSize = elementCurrentFontSize;
+                // Save each element and its selector in the `charges` array
                 charges.push([element, selector]);
             });
         });
